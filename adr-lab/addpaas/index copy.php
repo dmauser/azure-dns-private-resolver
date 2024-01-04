7949b4a497ecbd493bb1ee0d10c8eaae16a86f5c
Daniel Mauser <dmauser@hotmail.com>
2024-01-03 19:36:20 -0600

    update

---
+++ adr-lab/addpaas/index copy.php
@@ -0,0 +1,7 @@
+<?php
+print gmdate("Y-m-d\TH:i:s\Z");
+echo $variable1 ." ". $variable2;
+print $_SERVER['REMOTE_ADDR'];
+echo $variable1 ." ". $variable2;
+print $_SERVER['REMOTE_PORT']
+?>
---
+++ adr-lab/addpaas/index.php
@@ -0,0 +1,9 @@
+<?php
+print gmdate("Y-m-d\TH:i:s\Z");
+echo $variable1 ." ". $variable2;
+echo $_SERVER['HTTP_HOST'];
+echo $variable1 ." RMT Address: ". $variable2;
+print $_SERVER['REMOTE_ADDR'];
+echo $variable1 ."SRC PORT: ". $variable2;
+print $_SERVER['REMOTE_PORT']
+?>
